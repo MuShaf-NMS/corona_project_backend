5278ee0e97ce0ff0d8154c956443e0f60110b1a5
MuShaf-NMS <nurmuhammadshafa@gmail.com>
2020-06-17 20:45:12 +0800

    lanjut

--- app/controller/admin.py
+++ app/controller/admin.py
@@ -6,6 +6,9 @@ from datetime import datetime
 from passlib.hash import pbkdf2_sha256 as sha256
 import uuid
 
+def stringTime(dt):
+    return datetime.strptime(dt,"%Y-%m-%d")
+
 class Materi(Resource):
     @jwt_required
     def get(self):
@@ -23,8 +26,8 @@ class TambahAdmin(Resource):
     def post(self):
         now = datetime.now()
         data = request.get_json()
-        sql = """insert into user values(0,%s,%s,%s,%s,%s,%s)"""
-        params = [str(uuid.uuid4()),data["username"],sha256.hash(data["password"]),data["superadmin"],now,now]
+        sql = """insert into bio_user values(0,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+        params = [str(uuid.uuid4()),data["nama"],data["username"],sha256.hash(data["password"]),data["superadmin"],data["alamat"],data["tempat_lahir"],stringTime(data["tanggal_lahir"]),data["hp"],data["email"],now,now,str(uuid.uuid4())]
         return db.commit_data(sql,params)
 class Siswa(Resource):
     @jwt_required
